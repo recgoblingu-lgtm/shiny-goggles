ca17453462e1ae66c6e5c45a2bf26fbf1f0c00c4
recgoblingu-lgtm <recgoblingu@gmail.com>
2026-09-14 16:03:26 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.AURORA_WISP = "wss://kidsprepnest.org/vi/";
+window.AURORA_DISABLED_APPS = ["copilot","flashgames","gnmath","html5"];
+
+window.AURORA_SOUNDS = "https://www.myinstants.com/media/sounds";
